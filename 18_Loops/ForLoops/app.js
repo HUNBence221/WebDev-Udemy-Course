4238b4bbd7d464e2cceabee97a5bf44c7f902d1a
HUNBence221 <hun.bence221@gmail.com>
2025-05-14 14:13:23 +0200

    Importing the Loops Lecture's folder

--- 18_Loops/ForLoops/app.js
+++ 18_Loops/ForLoops/app.js
@@ -16,25 +16,39 @@
 // OUR FIRST FOR LOOP
 // ======================================
 
-for (let num = 1; num <= 10; num += 1) {
-    console.log(num);
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
 }
+// let i = 1; // start at 1
+// i <= 10; // stop when i is greater than 10
+// i++ // add 1 to i each time the loop runs
+// console.log(i); // print i to the console
+
+for (let i = 1; i <= 6; i++) {
+    console.log("Da ba dee da ba daa")
+}
+
 
 // ======================================
 // MORE EXAMPLES OF FOR LOOPS!
 // ======================================
+console.log("even loop")
+for (let i = 0; i < 21; i += 2) {
+    console.log(i);
+}
+console.log("odd loop")
+for(let i = 1; i < 20; i += 2) {
+    console.log(i);
+}
 
-// for (let i = 1; i <= 20; i += 2) {
-//     console.log(i)
-// }
-
-// for (let i = 100; i >= 0; i -= 10) {
-//     console.log(i);
-// }
-
-// for (let i = 10; i <= 1000; i *= 10) {
-//     console.log(i);
-// }
+console.log("-10 decreasing loop")
+for (let i = 100; i >= 0; i -= 10) {
+    console.log(i);
+}
+console.log("10 strong loop")
+for (let i = 10; i <= 1000; i *= 10) {
+    console.log(i);
+}
 
 // DO NOT RUN THIS LOOP
 // INFINITE LOOP!! BAD!!
