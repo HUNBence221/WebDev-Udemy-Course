5448238251adadf516bce8be6149a5a401f42b2a
HUNBence221 <hun.bence221@gmail.com>
2025-05-15 19:37:29 +0200

    updated the files

--- 18_Loops/ForLoops/app.js
+++ 18_Loops/ForLoops/app.js
@@ -50,9 +50,9 @@ for (let i = 10; i <= 1000; i *= 10) {
     console.log(i);
 }
 
-for (let i = 100; i >= 0; i /= 10) {
-    console.log(i);
-}
+// for (let i = 100; i >= 0; i /= 10) {
+//     console.log(i);
+// }
 
 // DO NOT RUN THIS LOOP
 // INFINITE LOOP!! BAD!!
