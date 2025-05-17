bb24ba25c674b4c83462dd5c358e1514dc90c3d6
HUNBence221 <hun.bence221@gmail.com>
2025-05-17 13:21:43 +0200

    ForOf loops: Creating the first for of loops plus syntax

--- 18_Loops/ForOf/app.js
+++ 18_Loops/ForOf/app.js
@@ -1,35 +1,40 @@
 const subreddits = ['cringe', 'books', 'chickens', 'funny', 'pics', 'soccer', 'gunners'];
 
-// for (let i = 0; i < subreddits.length; i++) {
-//     console.log(`Visit reddit.com/r/${subreddits[i]}`)
-// }
-
-for (let subreddit of subreddits) {
-    console.log(`Visit reddit.com/r/${subreddit}`)
+for (let i = 0; i < subreddits.length; i++) {
+    console.log(`Visit reddit.com/r/${subreddits[i]}`)
 }
 
+console.log("===============================================")
 
+for (let sub of subreddits) {
+    console.log(`Visit reddit.com/r/${sub}`);    
+}
 
-// const seatingChart = [
-//     ['Kristen', 'Erik', 'Namita'],
-//     ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
-//     ['Yuma', 'Sakura', 'Jack', 'Erika']
-// ]
+console.log("===============================================")
 
-// for (let i = 0; i < seatingChart.length; i++) {
-//     const row = seatingChart[i];
-//     for (let j = 0; j < row.length; j++) {
-//         console.log(row[j])
-//     }
-// }
+const seatingChart = [
+    ['Kristen', 'Erik', 'Namita'],
+    ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
+    ['Yuma', 'Sakura', 'Jack', 'Erika']
+]
 
-// for (let row of seatingChart) {
-//     for (let student of row) {
-//         console.log(student);
-//     }
-// }
+for (let i = 0; i < seatingChart.length; i++) {
+    const row = seatingChart[i];
+    for (let j = 0; j < row.length; j++) {
+        console.log(row[j])
+    }
+}
 
-// for (let char of "hello world") {
-//     console.log(char)
-// }
+console.log("===============================================")
 
+for (let row of seatingChart) {
+    for (let student of row) {
+        console.log(student);
+    }
+}
+
+console.log("===============================================")
+
+for (let char of 'hello world') {
+    console.log(char);
+}
