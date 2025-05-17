82b9348fd1aaf2b1780eac30199fad1aa1ac32bb
HUNBence221 <hun.bence221@gmail.com>
2025-05-17 13:06:03 +0200

    Bug Fixing (Now we can quit on the first try, and if we write a non number, it tells us to write a number, and doesn't make it as an attempt)

--- Guessing_Game_Project/app.js
+++ Guessing_Game_Project/app.js
@@ -7,16 +7,20 @@ while (!maximum) {
 const targetNum = Math.floor(Math.random() * maximum) + 1;
 console.log(targetNum);
 
-let guess = parseInt(prompt("Enter your first guess: "));
+let guess = prompt("Enter your first guess: (Type 'q' to quit)");
 let attempts = 1;
 
 while (parseInt(guess) !== targetNum) {
   if (guess === 'q') break;
-  attempts++;
+  guess = parseInt(guess);
   if (guess > targetNum) {
     guess = (prompt("Too high! Try again: "));
-  } else {
+    attempts++;
+  } else if (guess < targetNum) {
     guess = (prompt("Too low! Try again: "));
+    attempts++;
+  } else {
+    guess = (prompt("Invalid input! Please enter a number or 'q' to quit: "));
   }
 }
 
